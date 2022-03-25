4a186a421701186671107cf0fb74d1c5075c58a8
CrisCama <101947503+CrisCama@users.noreply.github.com>
2022-03-24 22:30:38 -0400

    Working
    3-24-2022

--- ToBeNamed/Assets/Scripts/In Battle/MouseManager.cs
+++ ToBeNamed/Assets/Scripts/In Battle/MouseManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MouseManager : MonoBehaviour
 {
@@ -13,17 +14,40 @@ public class MouseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Is Mouse over Unity UI element? -- it is so lets not do any custom mouse code.
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit))
         {
-            GameObject hitObject = hit.collider.transform.gameObject;
-            Debug.Log("hit" + hitObject.name);
+            GameObject hitObject = hit.collider.transform.parent.gameObject;
+            Debug.Log("MouseOver" + hitObject.name);
+            if(hitObject.GetComponent<Hex>() != null)
+            {
+                MouseOverHex(hitObject);
+            }
+        }
+    }
+    void MouseOverHex(GameObject hitObject)
+    {
+        //Debug.Log("hit" + hitObject.name);
 
+        if (Input.GetMouseButtonDown(0))
+        {
             MeshRenderer mr = hitObject.GetComponentInChildren<MeshRenderer>();
-            mr.material.color = Color.green;
+            if (mr.material.color == Color.green)
+            {
+                mr.material.color = Color.white;
+            }
+            else
+            {
+                mr.material.color = Color.green;
+            }
         }
     }
 }
